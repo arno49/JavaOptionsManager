86baf56dcc32aacf74c0e6ba4f18190406b2981e
aliaksei maiseyeu <>
2018-11-01 10:28:39 +0300

    fix 'E501 line too long'

--- shaper/lib/configi.py
+++ shaper/lib/configi.py
@@ -188,7 +188,10 @@ def write_yaml(path_to_file, data):
     """
     yaml.add_representer(OrderedDict, represent_ordereddict)
     if sys.version_info[0] == 2:
-        yaml.add_representer(unicode, represent_unicode)  # pylint: disable=undefined-variable
+        yaml.add_representer(
+            unicode,
+            represent_unicode
+        )  # pylint: disable=undefined-variable
 
     content = yaml.dump(
         data,
