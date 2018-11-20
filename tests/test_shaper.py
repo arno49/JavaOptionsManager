ea46ad4dcde88f33ea8dac836432ac4e7b0981eb
alexei-bogdanov <heavy.lexer@gmail.com>
2018-11-20 15:52:57 +0200

    compare only file internals in tests

--- tests/test_shaper.py
+++ tests/test_shaper.py
@@ -19,9 +19,13 @@ def test_read(test_assets_root):
     libs.parser.write(tree, output_file_path)
 
     output_data = libs.parser.read(output_file_path)
-    expected_data = libs.parser.read(expected_file_path)
+    output_data = manager.backward_path_parser(output_data)
 
-    assert output_data == expected_data
+    expected_data = libs.parser.read(expected_file_path)
+    expected_data = manager.backward_path_parser(expected_data)
+
+    for key, value in output_data.items():
+        assert expected_data[key] == value
 
     os.remove(output_file_path)
 
